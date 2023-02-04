3a1a1d97fe2c759244808bbb1b9cf1323dba8214
Lee Jia Rou <leejiarou1029@gmail.com>
2023-02-04 23:15:15 +0800

    change 'model4' to 'model'

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if option == 'Real-time Recognition':
         emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
         # load json and create model
-        json_file = open('./model/model4.json', 'r')
+        json_file = open('./model/model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         emotion_model = model_from_json(loaded_model_json)
