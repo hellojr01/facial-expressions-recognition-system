f9b920bf05bc2efef8b8da335ae34d382f6ded8e
Lee Jia Rou <leejiarou1029@gmail.com>
2023-02-06 20:31:55 +0800

    change model4 to model

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import cv2
 import streamlit as st
 import numpy as np
 from keras.models import model_from_json
+from streamlit_webrtc import webrtc_streamer
+from matplotlib import pyplot as plt
+from moviepy.editor import VideoFileClip
 import tempfile
+import threading
+import av
 
 st.set_page_config(page_title="FYP1",page_icon="😀")
 st.title("Facial Expressions Recognition System")
@@ -13,28 +18,46 @@ option = st.sidebar.selectbox(
 )
 FRAME_WINDOW = st.image([])
 
+lock = threading.Lock()
+img_container = {"img": None}
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    with lock:
+        img_container["img"] = img
+
+    return frame
+
 if option == 'Real-time Recognition':
-    start = st.checkbox('Start')
-    while start:
-        camera = cv2.VideoCapture(0)
-        _, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # start = st.checkbox('Start')
+    # while start:
+    camera = webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+    fig_place = st.empty()
+    fig, ax = plt.subplots(1, 1)
+
+    while camera.state.playing:
+        with lock:
+            img = img_container["img"]
+        if img is None:
+            continue
+        frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
         # load json and create model
-        json_file = open('./model/model4.json', 'r')
+        json_file = open('./model/model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         emotion_model = model_from_json(loaded_model_json)
 
         # load weights into new model
-        emotion_model.load_weights("./model/model4.h5")
+        emotion_model.load_weights("./model/model.h5")
 
         face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # detect faces available on camera
         num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+        emotion_count = {"Angry":0, "Disgusted":0, "Fearful":0, "Happy":0, "Neutral":0, "Sad":0, "Surprised":0}
 
         # take each face available on the camera and Preprocess it
         for (x, y, w, h) in num_faces:
@@ -45,33 +68,33 @@ if option == 'Real-time Recognition':
             # predict the emotions
             emotion_prediction = emotion_model.predict(cropped_img)
             maxindex = int(np.argmax(emotion_prediction))
+            emotion_count[emotion_dict[maxindex]] += 1
             cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
             FRAME_WINDOW.image(frame)
-    else:
-        st.write("Stopped")
+    # else:
+    #     st.write("Stopped")
 else:
     uploaded_video = st.file_uploader("Please upload a video.")
     if uploaded_video is not None:
         tfile = tempfile.NamedTemporaryFile(delete=False) 
         tfile.write(uploaded_video.read())
-  
-        camera = cv2.VideoCapture(tfile.name)
-        while camera.isOpened():
-            _, frame = camera.read()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        video = VideoFileClip(tfile.name)
+        for frame in video.iter_frames():
+            frames = cv2.cvtColor(np.array(frame), cv2.COLOR_BGR2RGB)
+    
             emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
             # load json and create model
-            json_file = open('./model/model4.json', 'r')
+            json_file = open('./model/model.json', 'r')
             loaded_model_json = json_file.read()
             json_file.close()
             emotion_model = model_from_json(loaded_model_json)
 
             # load weights into new model
-            emotion_model.load_weights("./model/model4.h5")
+            emotion_model.load_weights("./model/model.h5")
 
             face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray_frame = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
 
             # detect faces available on camera
             num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
